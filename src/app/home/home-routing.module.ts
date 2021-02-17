529674207c48979f2fbba3c90d361f8a810d60f0
Florianfifteen <florian1.robache@gmail.com>
2021-02-17 13:39:02 +0100

    Maj
    Maj

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent} from './shared/components/not-found/not-found.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: '**',
-    component: NotFoundComponent
   }
 ];
 
@@ -18,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeModule { }
